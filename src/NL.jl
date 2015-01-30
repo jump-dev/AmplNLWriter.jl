294dcffb03b05fad32b80524d28ecf804d36c49a
Jack Dunn <jack.dunn.nz@gmail.com>
2015-01-29 22:06:31 -0500

    Basic conversion of variables, numerics and operator codes

--- src/NL.jl
+++ src/NL.jl
@@ -3,6 +3,10 @@ module NL
 using MathProgBase
 importall MathProgBase.SolverInterface
 
+import Compat
+
+include("nl_params.jl")
+
 export NLSolver
 immutable NLSolver <: AbstractMathProgSolver
     options
@@ -69,7 +73,6 @@ MathProgBase.model(s::NLSolver) = NLMathProgModel()
 verify_support(c) = c
 
 function verify_support(c::Expr)
-    println(c)
     c
 end
 
@@ -124,8 +127,39 @@ end
 
 MathProgBase.setwarmstart!(m::NLMathProgModel, v::Vector{Float64}) = m.x_0 = v
 
+add_indent(c::String, indent::Int) = println(string(repeat(" ", indent), c))
+
+to_nl(m, c, indent::Int=0) = add_indent(string(c), indent)
+to_nl(m, c::Real, indent::Int=0) = add_indent("n$c", indent)
+function to_nl(m, c::Expr, indent::Int=0)
+    if c.head == :ref
+        if c.args[1] == :x
+            @assert isa(c.args[2], Int)
+            add_indent("v$(m.v_names[c.args[2]])", indent)
+        else
+            error("Unrecognized reference expression $c")
+        end
+    elseif c.head == :call
+        add_indent("o$(func_to_nl[c.args[1]])", indent)
+        for arg in c.args[2:end]
+            to_nl(m, arg, indent + 2)
+        end
+    else
+        println(string(repeat(" ", indent), c.head, " ", length(c.args)))
+        for arg in c.args
+            to_nl(m, arg, indent + 2)
+        end
+    end
+end
+
 function MathProgBase.optimize!(m::NLMathProgModel)
-    println("Not yet!")
+
+    m.v_names = ["$i" for i in 1:m.nvar]
+    m.c_names = ["$i" for i in 1:m.ncon]
+
+    m.constrs = map(1:m.ncon) do c
+        to_nl(m, MathProgBase.constr_expr(m.d, c))
+    end
 end
 
 MathProgBase.status(m::NLMathProgModel) = m.status
