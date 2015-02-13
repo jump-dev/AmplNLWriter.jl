679d970168b1699f89f9999fe10a9c00b15fd2ce
Jack Dunn <jack.dunn.nz@gmail.com>
2015-02-13 12:34:56 -0500

    Add trig function test

---
+++ examples/jump_nltrig.jl
@@ -0,0 +1,26 @@
+using JuMP, FactCheck
+import NL
+
+## Solve test problem with sind and cosd functions
+ #
+ #  min   (7 - (3*cosd(x1) + 5*cosd(x2)))^2 + (0 - (3*sind(x1) + 5*sind(x2)))^2
+ #  s.t.  x1, x2 continuous
+ #
+ #  The optimal objective value is 400..
+ ##
+
+m = Model(solver=NL.NLSolver())
+@defVar(m, x[1:2])
+
+@setNLObjective(m, Min, (7 - (3*cosd(x[1]) + 5*cosd(x[2])))^2 + (0 - (3*sind(x[1]) + 5*sind(x[2])))^2)
+
+facts("[jump_nltrig] Test optimal solutions") do
+  setValue(x[1], 30)
+  setValue(x[2], -50)
+  solve(m)
+  @fact getValue(x)[:] => roughly([38.21321, -21.78678], 1e-5)
+  setValue(x[1], -30)
+  setValue(x[2], 50)
+  solve(m)
+  @fact getValue(x)[:] => roughly([-38.21321, 21.78678], 1e-5)
+end
