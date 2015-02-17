14ed785de03e96b2f1093da8e4b4ba4e78bd170d
Jack Dunn <jack.dunn.nz@gmail.com>
2015-02-17 14:34:57 -0500

    Remove old prints

--- src/nl_convert.jl
+++ src/nl_convert.jl
@@ -30,15 +30,11 @@ function convert_formula(c::Expr)
       end
     # .nl has no n-ary multiplication so we need to convert to binary
     elseif op == :*
-      println("before:   $c")
       # Loop backwards over any more than binary *
       for i in length(c.args):-1:4
-        println("start $i: $c")
         arg = pop!(c.args)
-        println(arg)
         # Combine i-1 and ith terms into a binary * expression
         c.args[i - 1] = :($(c.args[i - 1]) * $arg)
-        println("end $i:   $c")
       end
     # Handle normal conversion cases
     else
