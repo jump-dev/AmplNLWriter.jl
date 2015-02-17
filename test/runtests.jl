fc43a2e3e3d6e656a38537f1f6e2eb10c6eec1c1
Jack Dunn <jack.dunn.nz@gmail.com>
2015-02-17 14:50:21 -0500

    Add hs tests and examples to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,21 @@ using FactCheck
 include("nl_convert.jl")
 
 import NL
+solver = NL.NLSolver()
 
 nlp_solvers = Any[]
-push!(nlp_solvers, NL.NLSolver())
+push!(nlp_solvers, solver)
 convex_nlp_solvers = Any[]
-push!(convex_nlp_solvers, NL.NLSolver())
+push!(convex_nlp_solvers, solver)
 minlp_solvers = Any[]
-push!(minlp_solvers, NL.NLSolver())
+push!(minlp_solvers, solver)
+
 
 include(joinpath(Pkg.dir("JuMP"), "test", "nonlinear.jl"))
+
+include(joinpath(Pkg.dir("JuMP"), "test", "hockschittkowski", "runhs.jl"))
+
+examples_path = joinpath(dirname(dirname(@__FILE__)), "examples")
+for example in readdir(examples_path)
+  include(joinpath(examples_path, example))
+end
