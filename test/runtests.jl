7431bed1aee5be0973ec97ce75f419af3a067553
Jack Dunn <jack.dunn.nz@gmail.com>
2015-02-17 13:36:11 -0500

    Run JuMP nonlinear tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,12 @@
 using FactCheck
 
 include("nl_convert.jl")
+
+import NL
+
+nlp_solvers = Any[]
+push!(nlp_solvers, NL.NLSolver())
+convex_nlp_solvers = Any[]
+push!(convex_nlp_solvers, NL.NLSolver())
+
+include(joinpath(Pkg.dir("JuMP"), "test", "nonlinear.jl"))
