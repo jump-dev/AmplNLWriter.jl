de9077fd1637bf195e836c33986fad0b65e898b1
Jack Dunn <jack.dunn.nz@gmail.com>
2015-02-18 21:04:02 -0500

    Disable couenne running on some tests it crashes on

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,10 +9,10 @@ couenne = NL.NLSolver("couenne", ["bonmin.lp_log_level"=>0,
 
 nlp_solvers = Any[]
 push!(nlp_solvers, bonmin)
-push!(nlp_solvers, couenne)
+# push!(nlp_solvers, couenne)
 convex_nlp_solvers = Any[]
 push!(convex_nlp_solvers, bonmin)
-push!(convex_nlp_solvers, couenne)
+# push!(convex_nlp_solvers, couenne)
 minlp_solvers = Any[]
 push!(minlp_solvers, bonmin)
 push!(minlp_solvers, couenne)
@@ -24,7 +24,7 @@ solver = bonmin
 include(joinpath(Pkg.dir("JuMP"), "test", "hockschittkowski", "runhs.jl"))
 
 examples_path = joinpath(dirname(dirname(@__FILE__)), "examples")
-for solver in nlp_solvers
+for solver in minlp_solvers
   println("With $(solver.solver_command)")
   for example in readdir(examples_path)
     include(joinpath(examples_path, example))
