bae4244ce9d0dc8aea5d5dcffd6fb428f27d5883
jcheyns <33093674+jcheyns@users.noreply.github.com>
2017-10-26 14:30:06 -0500

    Allow overwriting of temp files

--- src/AmplNLWriter.jl
+++ src/AmplNLWriter.jl
@@ -372,7 +372,7 @@ function optimize!(m::AmplNLMathProgModel)
     close(f_prob)
 
     # Rename file to have .nl extension (this is required by solvers)
-    mv(file_basepath, m.probfile)
+    mv(file_basepath, m.probfilere, move_destination=true)
 
     # Run solver and save exitcode
     t = time()
