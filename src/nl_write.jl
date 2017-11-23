1ae722113c71537e744669391b7d7a868e96725e
ccoffrin <carleton@coffrin.com>
2017-11-22 19:18:06 -0700

    better error message for user defined functions

--- src/nl_write.jl
+++ src/nl_write.jl
@@ -254,4 +254,11 @@ end
 
 nl_variable(index::Integer) = "v$index"
 nl_number(value::Real) = "n$value"
-nl_operator(operator::Symbol) = "o$(func_to_nl[operator])"
+
+function nl_operator(operator::Symbol)
+    if !(operator in keys(func_to_nl))
+        error("translation of the function \"$operator\" to NL is not defined")
+    end
+    return "o$(func_to_nl[operator])"
+end
+
