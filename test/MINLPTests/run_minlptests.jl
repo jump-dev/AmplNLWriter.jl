e9870aacda15976f351214036d497aed52d9daed
GregPlowman <GregPlowman@users.noreply.github.com>
2020-03-25 02:24:13 +1100

    Make model <: MOI.AbstractOptimizer (#99)
    * `@model` isa AbstractOptimizer
    
    * test model isa AbstractOptimizer
    
    * Bump MathOptInterface to 0.9.11
    
    * Get tests passing
    
    * Remove UniversalFallback
    
    * Fix infeasible tests
    
    * Bump Project version
    
    Co-authored-by: odow <o.dowson@gmail.com>

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -1,31 +1,47 @@
-using MINLPTests, JuMP, Ipopt, AmplNLWriter, Test
+import AmplNLWriter
+import Ipopt
+import MINLPTests
 
-const OPTIMIZER = MINLPTests.JuMP.with_optimizer(
-    AmplNLWriter.Optimizer, Ipopt.amplexe, ["print_level=0"]
-)
+using Test
+
+const OPTIMIZER = () -> AmplNLWriter.Optimizer(Ipopt.amplexe, ["print_level=0"])
 
 @testset "MINLPTests" begin
     ###
     ### src/nlp tests.
     ###
 
-    MINLPTests.test_nlp(OPTIMIZER, exclude = [
-        "005_011",  # Uses the function `\`
-        "006_010",  # User-defined function
-        "007_010"   # Infeasible model
-    ], objective_tol = 1e-5, primal_tol = 1e-5, dual_tol = NaN)
+    MINLPTests.test_nlp(
+        OPTIMIZER,
+        exclude = [
+            "005_011",  # Uses the function `\`
+            "006_010",  # User-defined function
+            "007_010",  # Infeasible model
+        ],
+        objective_tol = 1e-5,
+        primal_tol = 1e-5,
+        dual_tol = NaN,
+    )
 
     @testset "nlp_007_010" begin
-        MINLPTests.nlp_007_010(OPTIMIZER, 1e-5, NaN, NaN;
-            termination_target = MOI.INFEASIBLE,
-            primal_target = MOI.NO_SOLUTION)
+        MINLPTests.nlp_007_010(
+            OPTIMIZER,
+            1e-5,
+            NaN,
+            NaN,
+            Dict(MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.INFEASIBLE),
+            Dict(MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.NO_SOLUTION),
+        )
     end
 
     ###
     ### src/nlp-cvx tests.
     ###
 
-    MINLPTests.test_nlp_cvx(OPTIMIZER, exclude = [
-        "109_010"  # Ipopt fails to converge
-    ])
+    MINLPTests.test_nlp_cvx(
+        OPTIMIZER,
+        exclude = [
+            "109_010"  # Ipopt fails to converge
+        ]
+    )
 end
