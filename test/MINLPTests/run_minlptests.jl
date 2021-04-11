681cf1b36cb451aa6a74f00df3aea21d777e0ba5
odow <o.dowson@gmail.com>
2021-04-11 15:14:59 +1200

    Fix part II

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -14,7 +14,7 @@ const MOI = AmplNLWriter.MOI
 
 run_with_ampl() do path
     OPTIMIZER =
-        () -> MOI.CachingOptimizer(
+        () -> MOI.Utilities.CachingOptimizer(
             MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
             AmplNLWriter.Optimizer(path, ["print_level=0"]),
         )
