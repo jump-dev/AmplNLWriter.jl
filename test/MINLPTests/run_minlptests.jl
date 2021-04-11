2efa48ff6a6c9cd96d42255e9f0ab0154467ee9e
odow <o.dowson@gmail.com>
2021-04-11 17:43:02 +1200

    Fixes

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -6,7 +6,7 @@ import AmplNLWriter
 import Bonmin_jll
 import Couenne_jll
 import Ipopt_jll
-import MINLPTest
+import MINLPTests
 using Test
 
 const TERMINATION_TARGET = Dict(
@@ -16,13 +16,23 @@ const TERMINATION_TARGET = Dict(
 
 const PRIMAL_TARGET = Dict(
     MINLPTests.FEASIBLE_PROBLEM => AmplNLWriter.MOI.FEASIBLE_POINT,
-    MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.UNKNOWN_RESULT_STATUS,
+    MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.NO_SOLUTION,
 )
+
+# Reasons for exclusion:
+# nlp/005_011     : Uses the function `\`
+# nlp/006_010     : Uses a user-defined function
+# nlp/007_010     : Ipopt returns an infeasible point, not NO_SOLUTION.
+# nlp/008_010     : Couenne fails to converge
+# nlp/008_011     : Couenne fails to converge
+# nlp-cvx/109_010 : Ipopt fails to converge
+# nlp-cvx/206_010 : Couenne can't evaluate pow
+# nlp-mi/001_010  : Couenne fails to converge
 
 const CONFIG = Dict(
     "Bonmin" => Dict(
         "amplexe" => Bonmin_jll.amplexe,
-        "options" => ["bonmin.print_level=0"],
+        "options" => String["bonmin.nlp_log_level=0"],
         "tol" => 1e-5,
         "nlp_exclude" => ["005_011", "006_010"],
         "nlpcvx_exclude" => ["109_010"],
@@ -30,7 +40,7 @@ const CONFIG = Dict(
     ),
     "Couenne" => Dict(
         "amplexe" => Couenne_jll.amplexe,
-        "options" => [],
+        "options" => String[],
         "tol" => 1e-2,
         "nlp_exclude" => ["005_011", "006_010", "008_010", "008_011"],
         "nlpcvx_exclude" => ["109_010", "206_010"],
@@ -38,9 +48,9 @@ const CONFIG = Dict(
     ),
     "Ipopt" => Dict(
         "amplexe" => Ipopt_jll.amplexe,
-        "options" => ["print_level=0"],
+        "options" => String["print_level=0"],
         "tol" => 1e-5,
-        "nlp_exclude" => ["005_011", "006_010"],
+        "nlp_exclude" => ["005_011", "006_010", "007_010"],
         "nlpcvx_exclude" => ["109_010"],
         "nlpmi_exclude" => ["005_011", "006_010"],
     )
@@ -52,7 +62,7 @@ const CONFIG = Dict(
     @testset "NLP" begin
         MINLPTests.test_nlp(
             OPTIMIZER,
-            exclude = config["nlp_exlude"],
+            exclude = config["nlp_exclude"],
             termination_target = TERMINATION_TARGET,
             primal_target = PRIMAL_TARGET,
             objective_tol = config["tol"],
@@ -63,7 +73,7 @@ const CONFIG = Dict(
     @testset "NLP-CVX" begin
         MINLPTests.test_nlp_cvx(
             OPTIMIZER,
-            exclude = config["nlpcvx_exlude"],
+            exclude = config["nlpcvx_exclude"],
             termination_target = TERMINATION_TARGET,
             primal_target = PRIMAL_TARGET,
             objective_tol = config["tol"],
