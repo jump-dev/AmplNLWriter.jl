220fe0ec2ba2fb478cc99857758b251255cddcfc
Oscar Dowson <odow@users.noreply.github.com>
2021-04-12 08:47:49 +1200

    Add JLL solvers to MINLPTests (#126)

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -1,60 +1,98 @@
+if VERSION < v"1.3"
+    error("You must use Julia 1.3 or newer.")
+end
+
 import AmplNLWriter
+import Bonmin_jll
+import Couenne_jll
+import Ipopt_jll
 import MINLPTests
 using Test
 
-if VERSION < v"1.3"
-    import Ipopt
-    run_with_ampl(f) = f(Ipopt.amplexe)
-else
-    import Ipopt_jll
-    run_with_ampl(f) = Ipopt_jll.amplexe(f)
-end
+const TERMINATION_TARGET = Dict(
+    MINLPTests.FEASIBLE_PROBLEM => AmplNLWriter.MOI.LOCALLY_SOLVED,
+    MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.INFEASIBLE,
+)
 
-const MOI = AmplNLWriter.MOI
+const PRIMAL_TARGET = Dict(
+    MINLPTests.FEASIBLE_PROBLEM => AmplNLWriter.MOI.FEASIBLE_POINT,
+    MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.NO_SOLUTION,
+)
 
-run_with_ampl() do path
-    OPTIMIZER = MOI.OptimizerWithAttributes(
-        () -> MOI.Utilities.CachingOptimizer(
-            MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
-            AmplNLWriter.Optimizer(path),
-        ),
-        "print_level" => 0,
-    )
-    ###
-    ### src/nlp tests.
-    ###
+# Reasons for exclusion:
+# nlp/005_011     : Uses the function `\`
+# nlp/006_010     : Uses a user-defined function
+# nlp/007_010     : Ipopt returns an infeasible point, not NO_SOLUTION.
+# nlp/008_010     : Couenne fails to converge
+# nlp/008_011     : Couenne fails to converge
+# nlp-cvx/109_010 : Ipopt fails to converge
+# nlp-cvx/206_010 : Couenne can't evaluate pow
+# nlp-mi/001_010  : Couenne fails to converge
 
-    MINLPTests.test_nlp(
-        OPTIMIZER,
-        exclude = [
-            "005_011",  # Uses the function `\`
-            "006_010",  # User-defined function
-            "007_010",  # Infeasible model
-        ],
-        objective_tol = 1e-5,
-        primal_tol = 1e-5,
-        dual_tol = NaN,
-    )
+const CONFIG = Dict(
+    "Bonmin" => Dict(
+        "amplexe" => Bonmin_jll.amplexe,
+        "options" => String["bonmin.nlp_log_level=0"],
+        "tol" => 1e-5,
+        "nlp_exclude" => ["005_011", "006_010"],
+        "nlpcvx_exclude" => ["109_010"],
+        "nlpmi_exclude" => ["005_011", "006_010"],
+    ),
+    "Couenne" => Dict(
+        "amplexe" => Couenne_jll.amplexe,
+        "options" => String[],
+        "tol" => 1e-2,
+        "nlp_exclude" => ["005_011", "006_010", "008_010", "008_011"],
+        "nlpcvx_exclude" => ["109_010", "206_010"],
+        "nlpmi_exclude" => ["001_010", "005_011", "006_010"],
+    ),
+    "Ipopt" => Dict(
+        "amplexe" => Ipopt_jll.amplexe,
+        "options" => String["print_level=0"],
+        "tol" => 1e-5,
+        "nlp_exclude" => ["005_011", "006_010", "007_010"],
+        "nlpcvx_exclude" => ["109_010"],
+        "nlpmi_exclude" => ["005_011", "006_010"],
+    ),
+)
 
-    @testset "nlp_007_010" begin
-        MINLPTests.nlp_007_010(
+@testset "$(name)" for name in ["Ipopt", "Bonmin", "Couenne"]
+    config = CONFIG[name]
+    OPTIMIZER =
+        () -> AmplNLWriter.Optimizer(config["amplexe"], config["options"])
+    @testset "NLP" begin
+        MINLPTests.test_nlp(
             OPTIMIZER,
-            1e-5,
-            NaN,
-            NaN,
-            Dict(MINLPTests.INFEASIBLE_PROBLEM => AmplNLWriter.MOI.INFEASIBLE),
-            Dict(
-                MINLPTests.INFEASIBLE_PROBLEM =>
-                    AmplNLWriter.MOI.UNKNOWN_RESULT_STATUS,
-            ),
+            exclude = config["nlp_exclude"],
+            termination_target = TERMINATION_TARGET,
+            primal_target = PRIMAL_TARGET,
+            objective_tol = config["tol"],
+            primal_tol = config["tol"],
+            dual_tol = NaN,
         )
     end
-
-    ###
-    ### src/nlp-cvx tests.
-    ###
-
-    return MINLPTests.test_nlp_cvx(OPTIMIZER, exclude = [
-        "109_010",  # Ipopt fails to converge
-    ])
+    @testset "NLP-CVX" begin
+        MINLPTests.test_nlp_cvx(
+            OPTIMIZER,
+            exclude = config["nlpcvx_exclude"],
+            termination_target = TERMINATION_TARGET,
+            primal_target = PRIMAL_TARGET,
+            objective_tol = config["tol"],
+            primal_tol = config["tol"],
+            dual_tol = NaN,
+        )
+    end
+    if name != "Ipopt"
+        @testset "NLP-MI" begin
+            MINLPTests.test_nlp_mi(
+                OPTIMIZER,
+                exclude = config["nlpmi_exclude"],
+                termination_target = TERMINATION_TARGET,
+                primal_target = PRIMAL_TARGET,
+                objective_tol = config["tol"],
+                primal_tol = config["tol"],
+                dual_tol = NaN,
+            )
+        end
+    end
 end
