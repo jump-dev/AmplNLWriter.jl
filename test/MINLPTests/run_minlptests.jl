cbe1280601aa6e3979b5b2c5eb6dcff1525d1a91
odow <o.dowson@gmail.com>
2022-04-29 11:22:01 +1200

    Add copyright headers

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2015: AmplNLWriter.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 import AmplNLWriter
 import MINLPTests
 using Test
