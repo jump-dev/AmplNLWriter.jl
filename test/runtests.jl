e3292be1706f05e98e96e1c27a095df92afce543
Oscar Dowson <odow@users.noreply.github.com>
2022-04-30 15:56:41 +1200

    Add copyright headers (#155)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2015: AmplNLWriter.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 using Test
 
 @testset "MOI_wrapper" begin
