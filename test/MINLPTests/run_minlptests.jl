075653c52328fc4edfc86ea306433ef765b4f572
odow <o.dowson@gmail.com>
2022-05-23 14:45:12 +1200

    Try re-enabling SHOT (II)

--- test/MINLPTests/run_minlptests.jl
+++ test/MINLPTests/run_minlptests.jl
@@ -98,7 +98,7 @@ CONFIG["SHOT"] = Dict(
     "infeasible_point" => AmplNLWriter.MOI.UNKNOWN_RESULT_STATUS,
 )
 
-@testset "$(name)" for name in ["Ipopt", "Bonmin", "Couenne"]
+@testset "$(name)" for name in ["Ipopt", "Bonmin", "Couenne", "SHOT"]
     config = CONFIG[name]
     OPTIMIZER =
         () -> AmplNLWriter.Optimizer(config["amplexe"], config["options"])
